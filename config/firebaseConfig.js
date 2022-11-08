3fd4067f402f883ed67ba7435eb2ea61b8af336c
berishaedi <berisha.edi@outlook.com>
2022-11-08 15:20:20 +0100

    emailAuth

---
+++ config/firebaseConfig.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAX3sX5livg-yr3uy2VAjNbndV2raWe7wM",
+  authDomain: "todo-list-80c83.firebaseapp.com",
+  projectId: "todo-list-80c83",
+  storageBucket: "todo-list-80c83.appspot.com",
+  messagingSenderId: "10393103976",
+  appId: "1:10393103976:web:a619cf269542ac0b2c15dd"
+};
+
+// Initialize Firebase
+let app;
+if(firebase.apps.lenght === 0){
+    app = firebase.initializeApp(firebaseConfig);
+}else {
+    app = firebase.app()
+}
+
+
+const auth = firebase.auth()
+
+export {auth};
---
+++ pages/LoginPage.js
@@ -0,0 +1,95 @@
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+
+
+
+
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("")
+  const [passwort, setPasswort] = useState("")
+  
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Email'
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Passwort'
+          value={passwort}
+          onChangeText={text => setPasswort(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+
+
+    </KeyboardAvoidingView>
+  )
+}
+
+export default LoginPage
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input:{
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 10,
+  },
+  buttonContainer:{
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  button:{
+    backgroundColor:"#0782f9",
+    width:"100%",
+    padding : 15,
+    borderRadius:15,
+    margin: 2,
+  },
+  buttonOutline:{
+    backgroundColor: "white",
+    marginTop: 5,
+  },
+  buttonText:{
+
+  },
+  buttonOutlineText:{
+
+  }
+
+})
